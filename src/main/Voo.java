2a83a3ec92d7eca71f9db213ba929086359e4a23
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-08-21 11:40:44 -0300

    Feat: implementa classe Voo que possui metodos que calculam o tempo de voo e retornam uma string com os dados desse calculo

--- src/main/Voo.java
+++ src/main/Voo.java
@@ -1,4 +1,33 @@
 package src.main;
 
+/**
+ * Classe Voo.
+ */
+
 public class Voo {
+  private int tempoVoo;
+
+  /**
+   * Método para retornar tempo de Voo.
+   *
+   */
+  public int retornarTempoVoo(double distanciaKm) {
+
+    Double duracaoDouble = distanciaKm / 700.0;
+    int round = (int) Math.round(duracaoDouble);
+    this.tempoVoo = round;
+    return this.tempoVoo;
+  }
+
+  /**
+   * Método para retornar informaçao do Voo.
+   *
+   */
+  public String retornarInformacaoVoo(String embarque, String origem, String desembarque,
+                                      String destino) {
+    return "Voo de " + origem
+            + "para " + destino + "saindo ás "
+            + embarque + "e chegando ao destino às " + desembarque;
+  }
 }
+
